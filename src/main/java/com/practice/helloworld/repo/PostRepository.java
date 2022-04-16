fd493ee509f42cdacc18438b8fe0b8c854f45b51
Gorobchick <98554586+Gorobchick@users.noreply.github.com>
2022-04-16 20:46:33 +0300

    Add files via upload

---
+++ src/main/java/com/practice/helloworld/repo/PostRepository.java
@@ -0,0 +1,9 @@
+package com.practice.helloworld.repo;
+
+import com.practice.helloworld.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+}
