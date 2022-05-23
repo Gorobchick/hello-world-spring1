91fb34cdd4c1b497d9c2a117207dfd6702928979
Gorobchick <98554586+Gorobchick@users.noreply.github.com>
2022-05-23 11:09:25 +0300

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.helloworld.repo;
-
-import com.practice.helloworld.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
