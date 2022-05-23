aaa02aecfebc7317f12685fda31d26a0de90fda7
Oleksander <o.cropiva@gmail.com>
2022-05-23 12:06:58 +0300

    reg

--- src/main/java/com/practice/helloworld/repo/PostRepository.java
+++ src/main/java/com/practice/helloworld/repo/PostRepository.java
@@ -3,5 +3,5 @@ package com.practice.helloworld.repo;
 import com.practice.helloworld.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends CrudRepository< Post, Long> {
 }
